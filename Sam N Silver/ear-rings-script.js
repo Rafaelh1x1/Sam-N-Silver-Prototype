bca54a36d32f76507c28beed9a0bd92eca73661e
Rafael Hung <111296938+Rafaelh1x1@users.noreply.github.com>
2023-02-08 11:14:00 -0500

    Add files via upload

---
+++ Sam N Silver/ear-rings-script.js
@@ -0,0 +1,13 @@
+const btnNavEl = document.querySelector(".hamburger");
+const headerEl = document.querySelector(".ear-rings-page");
+const btncloseEl = document.querySelector(".x");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+btncloseEl.addEventListener("click", function () {
+  headerEl.classList.add("nav-open");
+});
+
+("nav-open");
